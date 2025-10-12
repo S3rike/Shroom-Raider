7da392c86e85908500a129a828f7407f8f0a4441
John Doe <enzoeoh@protonmail.com>
2025-10-12 17:17:04 +0800

    idk what to put

--- shroom_raider.py
+++ shroom_raider.py
@@ -1,29 +1,34 @@
 import sys
 
-input = sys.stdin.readline
-
 def run():
     while(display()): pass
 
 def display():
     current_map, row, column = fetch_map()
+    for row in current_map:
+        print("".join(row))
+
+def menu():
+    file_name = input("Enter path of map (etc. maps/Sample.txt): ")
+    '''
+    function intended for menu/map selection
+    '''
+    ...
+    if FileNotFoundError:
+        print("File not found, try again")
+        file_name = input("Enter path of map (etc. maps/Sample.txt): ")
+    return open(file_name, "rt")
 
 def fetch_map(file = menu()):
     map = [list(row) for row in file.read().split("\n")]
+    file.close()
     '''
     you may add addl checks if map is legal
     '''
-    return map, len(map), len(len(map))
-
-def menu():
-    file_name = "maps/Sample.txt"
-    '''
-    function intended for menu
-    currently just returns sample.txt
-    '''
-    ...
-    return open(file_name, "rt")
-
+    if not map:
+        return [], 0, 0
+    
+    return map, len(map), len(map[0])
 
 if __name__ == "__main__":
     run()
