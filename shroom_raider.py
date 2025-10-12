c06d7c92823d65a8a54252148fab5db59752ad7f
S3rike <jrcamina@up.edu.ph>
2025-10-12 15:01:46 +0800

    Input Done including file handling

--- shroom_raider.py
+++ shroom_raider.py
@@ -1,8 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
 def run():
-    display()
+    while(display()): pass
 
 def display():
+    current_map, row, column = fetch_map()
+
+def fetch_map(file = menu()):
+    map = [list(row) for row in file.read().split("\n")]
+    '''
+    you may add addl checks if map is legal
+    '''
+    return map, len(map), len(len(map))
+
+def menu():
+    file_name = "maps/Sample.txt"
+    '''
+    function intended for menu
+    currently just returns sample.txt
+    '''
     ...
+    return open(file_name, "rt")
+
 
 if __name__ == "__main__":
     run()
